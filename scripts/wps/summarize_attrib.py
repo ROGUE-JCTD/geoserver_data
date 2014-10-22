485aac7d0902027050b08b48ef0a9e3b2362fe8d
Syrus Mesdaghi <syrusm@gmail.com>
2014-10-22 14:02:26 +0000

    added simple json py lib. also improvements to summarize wps. works for number and strings

--- scripts/wps/summarize_attrib.py
+++ scripts/wps/summarize_attrib.py
@@ -1,33 +1,83 @@
-import math
 from geoserver.wps import process
 from geoscript.geom import Point
 from geoscript.feature import Feature
 from geoscript.layer import Layer
 from java.lang import String
-from geoscript.feature.field import Field
+from org.apache.commons.math3.stat.descriptive import DescriptiveStatistics
+from org.apache.commons.math3.stat.descriptive import SummaryStatistics
+import simplejson as json
 
 @process(
-  title = 'Summarize Attribute Values',
-  description = 'summarize the values for a particular attribute in the provided features.',
-  inputs = {
-    'features': (Layer, 'Features to which distance and bearing should be calculated.'),
-    'attributeName': (String, 'Attribute to compute stats for')
-  },
-  outputs = {
-    'result': (String, 'json string containing the stats.')
-  }
+    title = 'Summarize Attribute Values',
+    description = 'summarize the values for a particular attribute in the provided features.',
+    inputs = {
+        'features': (Layer, 'Features to which distance and bearing should be calculated.'),
+        'attributeName': (String, 'Attribute to compute stats for')
+    },
+    outputs = {
+        'result': (String, 'json string containing the stats.')
+    }
 )
 
-# pass in Field we'll have to aggregate
 def run(features, attributeName):
-  stats = {
-    'populatedCount': 0,
-    'totalCount': 0
-  }
 
-  for f in features.features():
-    stats['totalCount'] += 1
-    if f[attributeName] is not None: 
-      stats['populatedCount'] += 1
-  
-  return '{"totalCount":' + str(stats['totalCount']) + ', "populatedCount":' + str(stats['populatedCount']) + '}'
+    #stats = {
+    #  type: 'number',
+    #  populatedCount: 1462,
+    #  totalCount: 0,
+    #  uniqueValues: {
+    #    '10.5': 4,
+    #    '9.7': 1,
+    #  },
+    #  min: 36.618,
+    #  max: 86.2747,
+    #  range: 49.6567,
+    #  sum: 106165,
+    #  mean: 72.6165,
+    #  median: 74.2404,
+    #  stdDev: 15.324,
+    #  coefficient: 0.0123
+    #}
+    ds = DescriptiveStatistics()
+    ss = SummaryStatistics()
+
+    stats = {
+        'type': 'number',
+        'populatedCount': 0,
+        'totalCount': 0,
+        'uniqueValues': {}
+    }
+
+    field_type_is_number = True
+
+    for f in features.features():
+        stats['totalCount'] += 1
+        val = f.attributes[attributeName]
+
+        if val in stats['uniqueValues']:
+            stats['uniqueValues'][val] += 1
+        else:
+            stats['uniqueValues'][val] = 1
+
+        if val is not None and val != '':
+            stats['populatedCount'] += 1
+
+            try:
+                val_float = float(val)
+                ds.addValue(val_float)
+                ss.addValue(val_float)
+            except ValueError:
+                field_type_is_number = False
+
+    if field_type_is_number is True:
+        stats['min'] = ss.getMin()
+        stats['max'] = ss.getMax()
+        stats['range'] = stats['max'] - stats['min']
+        stats['sum'] = ss.getSum()
+        stats['mean'] = ss.getMean()
+        stats['median'] = ds.getPercentile(50)
+        stats['stdDev'] = ds.getStandardDeviation()
+    else:
+        stats['type'] = 'string'
+
+    return json.dumps(stats)
